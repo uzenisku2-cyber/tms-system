600bfae624d59de74e3cf281c329d531d2ba042c
Miloš Kökörčený <uzenisku1@seznam.cz>
2026-07-05 00:19:44 +0200

    chore: remove PHPStan cache from repository

--- backend/storage/phpstan/cache/PHPStan/c3/7f/c37fb9226c4a9487f23171e8b2d8706767b518cce6a28474d3e2f515425ba326.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types = 1);
-
-// odsl-/var/www/backend/vendor/composer/../phar-io/version/src/
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => 'v1-enums',
-   'data' => 
-  array (
-  ),
-));
